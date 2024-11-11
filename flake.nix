a2136a109a74d4e6000f47d8febf7734b431bcc2
meml0rz <meml0rz>
2024-11-12 00:09:45 +0100

    kekl

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
         nixpkgs-stable.url = "nixpkgs/nixos-24.05";
         nixpkgs.url = "nixpkgs/nixos-unstable";
         home-manager.url = "github:nix-community/home-manager/release-24.05";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
         nixvim = {
             url = "github:nix-community/nixvim";
             inputs.nixpkgs.follows = "nixpkgs";
