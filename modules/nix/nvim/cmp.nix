ea13297b188e6dec8c17288b9ef82511ec3a18ee
meml0rz <meml0rz>
2024-11-12 23:06:28 +0100

    kekl

--- modules/nix/nvim/cmp.nix
+++ modules/nix/nvim/cmp.nix
@@ -7,13 +7,28 @@
 		cmp = {
 			enable = true;
       		autoEnableSources = true;
-			settings = {
-				sources = [
-					{name = "nvim_lsp";}
-					{name = "path";}
-					{name = "buffer";}
-					{name = "luasnip";}
-      			];
+            settings = {
+				mapping = {
+                    "<C-Space>" = "cmp.mapping.complete()";
+                    "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+                    "<C-e>" = "cmp.mapping.close()";
+                    "<C-f>" = "cmp.mapping.scroll_docs(4)";
+                    "<CR>" = "cmp.mapping.confirm({ select = true })";
+                    "<S-Tab>" = {
+                        action = "cmp.mapping.select_prev_item()";
+                        modes = [
+                            "i"
+                            "s"
+                        ];
+                    };
+                    "<Tab>" = {
+                        action = "cmp.mapping.select_next_item()";
+                        modes = [
+                            "i"
+                            "s"
+                        ];
+                    };
+                };
 			};
 		};
 	};
