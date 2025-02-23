5443a410166d41f216b34b9d51da1026cda60dea
Sojus07 <Sojus07>
2025-02-23 14:35:21 +0100

    kekl

--- hosts/meteor/hardware-configuration.nix
+++ hosts/meteor/hardware-configuration.nix
@@ -23,16 +23,15 @@
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
-
+  
+  fileSystems."/media/STUFF" = 
+    { device = "/dev/disk/by-label/STUFF";
+      fsType = "ext4";
+    };
+  
   swapDevices = [ ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
